11cbde88ded362aab327cc1803167e788224e2cd
softvision.agustin.ramos <agustin.ramos@softvision.com>
2021-08-22 20:31:17 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# ReactPwa Demo App
+
+This project was generated with npx create-react-app
+
+## Para probar la app
+
+npm install
+
+npm install -g serve
+
+npm run build
+
+serve -s build
+
+
--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <img src="/nrd.svg" alt="wcimage"></img>
+          Nerdearla Octubre 2021
+          <a target="_blank" href="https://nerdear.la/en/" rel="noreferrer">Ir a Nerdearla</a>
       </header>
     </div>
   );
